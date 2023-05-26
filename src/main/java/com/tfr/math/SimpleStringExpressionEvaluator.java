f7540a94a6e1abaa02498105948bce425dd3d3e9
Erik Hage <ehage4@gmail.com>
2023-05-26 13:08:27 -0400

    Add simple version of string expression evaluator

---
+++ src/main/java/com/tfr/math/SimpleStringExpressionEvaluator.java
@@ -0,0 +1,88 @@
+package com.tfr.math;
+
+import java.util.Set;
+import java.util.Stack;
+
+public class SimpleStringExpressionEvaluator {
+
+    private static final Set<Character> operators = Set.of('+', '-', '*', '/');
+
+    public static double evaluate(String expression) {
+        char[] tokens = expression.toCharArray();
+
+        Stack<Double> values = new Stack<>();
+        Stack<Character> operations = new Stack<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i] == ' ') {
+                continue;
+            }
+
+            if (isNumber(tokens[i])) {
+                StringBuilder sb = new StringBuilder();
+                while (i < tokens.length && isNumber(tokens[i])) {
+                    sb.append(tokens[i++]);
+                }
+                values.push(Double.parseDouble(sb.toString()));
+                printStacks(values, operations);
+                i--;
+            } else if (isOperator(tokens[i])) {
+                while (!operations.isEmpty() && hasPrecedence(tokens[i], operations.peek())) {
+                    double result = applyOperation(operations.pop(), values.pop(), values.pop());
+                    values.push(result);
+                    printStacks(values, operations);
+                }
+                operations.push(tokens[i]);
+                printStacks(values, operations);
+            }
+        }
+
+        while (!operations.isEmpty()) {
+            double result = applyOperation(operations.pop(), values.pop(), values.pop());
+            values.push(result);
+            printStacks(values, operations);
+        }
+
+        return values.pop();
+    }
+
+    private static boolean hasPrecedence(char operator1, char operator2) {
+        if ((operator1 == '*' || operator1 == '/') && (operator2 == '+' || operator2 == '-')) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isNumber(char token) {
+        boolean isNumber = token >= '0' && token <= '9';
+        boolean isDecimal = token == '.';
+
+        return isNumber || isDecimal;
+    }
+
+    private static boolean isOperator(char token) {
+        return operators.contains(token);
+    }
+
+    private static double applyOperation(char op, double b, double a) {
+        switch (op) {
+            case '+' -> { return a + b; }
+            case '-' -> { return a - b; }
+            case '*' -> { return a * b; }
+            case '/' -> {
+                if (b == 0) {
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                }
+                return a / b;
+            }
+        }
+
+        return 0;
+    }
+
+    private static void printStacks(Stack<Double> values, Stack<Character> operators) {
+        System.out.println("vals: " + values);
+        System.out.println("ops : " + operators);
+    }
+
+}
---
+++ src/test/java/com/tfr/math/SimpleStringExpressionEvaluatorTest.java
@@ -0,0 +1,92 @@
+package com.tfr.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleStringExpressionEvaluatorTest {
+
+    @Test
+    public void testEvaluate_GivenAddition_ExpectSum() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 + 2");
+
+        assertEquals(12, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenAdditionWithDecimal_ExpectSum() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10.5 + 2.1");
+
+        assertEquals(12.6, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenSubtraction_ExpectDifference() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 - 2");
+
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenSubtractionWithDecimal_ExpectDifference() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10.2 - 2.1");
+
+        assertEquals(8.1, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenMultiplication_ExpectProduct() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 * 2");
+
+        assertEquals(20, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenMultiplicationWithDecimal_ExpectProduct() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10.5 * 2");
+
+        assertEquals(21, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenDivision_ExpectFraction() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 / 2");
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenDivisionWithDecimal_ExpectFraction() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 / 2.5");
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenAdditionAndMultiplicationOperators_ExpectOrderOfOperationsRespected() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 + 2 * 6");
+
+        assertEquals(22, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenAdditionAndMultiplicationOperators2_ExpectOrderOfOperationsRespected() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("100 * 2 + 12");
+
+        assertEquals(212, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenSubtractionAndMultiplicationOperators_ExpectOrderOfOperationsRespected() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("10 - 2 * 6");
+
+        assertEquals(-2, result);
+    }
+
+    @Test
+    public void testEvaluate_GivenAllOperators_ExpectOrderOfOperationsRespected() {
+        Double result = SimpleStringExpressionEvaluator.evaluate("5 * 10 - 2 * 6 + 100");
+
+        assertEquals(138, result);
+    }
+}
