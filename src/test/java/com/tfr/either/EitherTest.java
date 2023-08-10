eb9f68e864c85e2297bdca0725129fe80606c801
Erik Hage <ehage4@gmail.com>
2023-08-10 00:01:35 -0400

    add Either unit test

--- src/test/java/com/tfr/either/EitherTest.java
+++ src/test/java/com/tfr/either/EitherTest.java
@@ -8,42 +8,51 @@ class EitherTest {
 
     @Test
     void shouldReturnALeftInstance() {
-        Either<String, Integer> result = Either.left("a");
+        Either<String, Integer> either = Either.left("a");
 
-        assertTrue(result instanceof Left);
+        assertTrue(either instanceof Left);
     }
 
     @Test
     void shouldReturnARightInstance() {
-        Either<String, Integer> result = Either.right(1);
+        Either<String, Integer> either = Either.right(1);
 
-        assertTrue(result instanceof Right);
+        assertTrue(either instanceof Right);
     }
 
     @Test
     void shouldMapLeft() {
-        Either<String, Integer> result = Either.left("a");
+        Either<String, Integer> either = Either.left("a");
 
-        Either<String, Integer> mapResult = result.mapLeft(left -> left + "b");
+        Either<String, Integer> result = either.mapLeft(left -> left + "b");
 
-        assertEquals("ab", mapResult.getLeft().orElse("c"));
+        assertEquals("ab", result.getLeft().orElse("c"));
     }
 
     @Test
     void shouldMapRight() {
-        Either<String, Integer> result = Either.right(10);
+        Either<String, Integer> either = Either.right(10);
 
-        Either<String, Integer> mapResult = result.map(r -> r + 2);
+        Either<String, Integer> result = either.map(r -> r + 2);
 
-        assertEquals(12, mapResult.getRight().orElse(100));
+        assertEquals(12, result.getRight().orElse(100));
     }
 
     @Test
     void shouldFlatMapLeft() {
-        Either<String, Integer> result = Either.left("abcd");
+        Either<String, Integer> either = Either.left("abcd");
 
-        Either<Integer, Integer> mapped = result.flatMapLeft(left -> Either.left(left.length()));
+        Either<Integer, Integer> result = either.flatMapLeft(left -> Either.left(left.length()));
 
-        assertEquals(4, mapped.getLeft().orElse(100));
+        assertEquals(4, result.getLeft().orElse(100));
+    }
+
+    @Test
+    void shouldFlatMapRight() {
+        Either<String, Integer> either = Either.right(8);
+
+        Either<String, Double> result = either.flatMap(right -> Either.right(right * 1.5));
+
+        assertEquals(12, result.getRight().orElse(77.5));
     }
 }
