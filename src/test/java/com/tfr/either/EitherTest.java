c2726abad7a362cbde45375c246cddb702bda66e
Erik Hage <ehage4@gmail.com>
2023-08-11 10:28:51 -0400

    [Erik] Add Either unit test

--- src/test/java/com/tfr/either/EitherTest.java
+++ src/test/java/com/tfr/either/EitherTest.java
@@ -2,6 +2,8 @@ package com.tfr.either;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EitherTest {
@@ -54,5 +56,19 @@ class EitherTest {
         Either<String, Double> result = either.flatMap(right -> Either.right(right * 1.5));
 
         assertEquals(12, result.getRight().orElse(77.5));
+    }
+
+    @Test
+    void shouldFilterLeft() {
+        Either<String, Integer> either = Either.left("abcd");
+
+        Either<String, Integer> result = either.filterLeft((left) -> {
+            if ("abcd".equals(left)) {
+              return Optional.of(10);
+            }
+            return Optional.empty();
+        });
+
+        assertEquals(10, result.getRight().orElse(1));
     }
 }
