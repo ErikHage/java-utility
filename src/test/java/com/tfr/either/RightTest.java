6054293260b8e133cde224b987d10a146176d9ce
Erik Hage <ehage4@gmail.com>
2023-08-15 00:44:15 -0400

    add test

--- src/test/java/com/tfr/either/RightTest.java
+++ src/test/java/com/tfr/either/RightTest.java
@@ -7,6 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class RightTest {
 
     @Test
+    void shouldMap() {
+        Either<String, Integer> right = Either.right(10);
+
+        Either<String, Double> result = right.map(i -> i * 1.5);
+
+        assertEquals(15.0, result.getRight().orElse(0.0));
+    }
+
+    @Test
     void shouldBeRight() {
         Either<String, Integer> right = Either.right(10);
 
