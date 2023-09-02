7034596e607ebe4f021bb1e87af22c6d3db1b6ff
Erik Hage <ehage4@gmail.com>
2023-09-02 08:59:47 -0400

    add test

--- src/test/java/com/tfr/either/LeftTest.java
+++ src/test/java/com/tfr/either/LeftTest.java
@@ -2,16 +2,29 @@ package com.tfr.either;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeftTest {
 
     @Test
-    void shouldMapRight() {
+    void shouldNotMapLeftValue() {
         Either<String, Integer> either = Either.left("string");
 
         Either<String, Double> result = either.map(r -> r * 1.1);
 
         assertEquals("string", result.getLeft().orElse("not string"));
+        assertEquals(Optional.empty(), result.getRight());
+    }
+
+    @Test
+    void shouldNotMapRightValue() {
+        Either<String, Integer> either = Either.left("string");
+
+        Either<String, Double> result = either.map(r -> r * 1.1);
+
+        assertEquals("string", result.getLeft().orElse("not string"));
+        assertEquals(Optional.empty(), result.getRight());
     }
 }
