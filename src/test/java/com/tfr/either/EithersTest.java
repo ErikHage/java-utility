43a376608cf4e301eb1afd0220c233fb4101076c
Erik Hage <ehage4@gmail.com>
2023-10-14 22:22:07 -0400

    add Eithers utility test

--- src/test/java/com/tfr/either/EithersTest.java
+++ src/test/java/com/tfr/either/EithersTest.java
@@ -2,12 +2,19 @@ package com.tfr.either;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EithersTest {
 
     @Test
-    void shouldGetFirstFailure() {
-        // todo
+    void shouldGetFirstFailureWhenRight() {
+        Either<String, Integer> either = Either.right(1);
+
+        Either<String, List<Integer>> result = Optional.of(either).stream().collect(Eithers.firstFailure());
+
+        assertEquals(List.of(1), result.getRight().orElse(List.of()));
     }
 }
