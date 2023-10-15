75c760c954cf0e16a588cbe656320d6cec89536e
Erik Hage <ehage4@gmail.com>
2023-10-14 22:23:54 -0400

    remove extra newline

--- src/test/java/com/tfr/either/EithersTest.java
+++ src/test/java/com/tfr/either/EithersTest.java
@@ -15,8 +15,7 @@ class EithersTest {
                 Either.right(1),
                 Either.right(2)
         );
-
-
+        
         Either<String, List<Integer>> result = eithers.stream().collect(Eithers.firstFailure());
 
         assertEquals(List.of(1, 2), result.getRight().orElse(List.of()));
