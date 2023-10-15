2a03f313f0d9639c6389cf070276296b7c36150f
Erik Hage <ehage4@gmail.com>
2023-10-14 22:24:14 -0400

    remove unused import

--- src/test/java/com/tfr/either/EithersTest.java
+++ src/test/java/com/tfr/either/EithersTest.java
@@ -3,7 +3,6 @@ package com.tfr.either;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,7 +14,7 @@ class EithersTest {
                 Either.right(1),
                 Either.right(2)
         );
-        
+
         Either<String, List<Integer>> result = eithers.stream().collect(Eithers.firstFailure());
 
         assertEquals(List.of(1, 2), result.getRight().orElse(List.of()));
