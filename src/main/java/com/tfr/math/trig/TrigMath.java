db910b29e0c173077612639dc60d37c87bceeda4
Erik Hage <ehage4@gmail.com>
2023-10-26 20:42:54 -0400

    add javadocs

--- src/main/java/com/tfr/math/trig/TrigMath.java
+++ src/main/java/com/tfr/math/trig/TrigMath.java
@@ -4,13 +4,18 @@ public class TrigMath {
 
     /**
      * Convert angle in degrees to angle in radians
-     * @param degrees angle in degrees
+     * @param degrees - angle in degrees
      * @return angle in radians
      */
     public static double toRadians(double degrees) {
         return degrees * (Math.PI/180);
     }
 
+    /**
+     * Convert angle in radian to angle in degrees
+     * @param radians - angle in radians
+     * @return angle in degrees
+     */
     public static double toDegrees(double radians) {
         return radians * (180/Math.PI);
     }
