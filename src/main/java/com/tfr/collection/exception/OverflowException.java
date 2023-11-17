2ad758f555bfaad7309f004309ff2e69c0b78cb8
Erik Hage <ehage4@gmail.com>
2023-11-16 20:48:36 -0500

    add javadoc

--- src/main/java/com/tfr/collection/exception/OverflowException.java
+++ src/main/java/com/tfr/collection/exception/OverflowException.java
@@ -2,6 +2,10 @@ package com.tfr.collection.exception;
 
 public class OverflowException extends RuntimeException {
 
+    /**
+     * Thrown when a stack overflow occurs because an application recurses too deeply.
+     * @param message String
+     */
     public OverflowException(String message) {
         super(message);
     }
