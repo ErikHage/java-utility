ca3cf86816e484ee7438a271ff635b8fb041a75c
Erik Hage <ehage4@gmail.com>
2023-12-05 21:58:27 -0500

    javadocs

--- src/main/java/com/tfr/math/vector/Vector.java
+++ src/main/java/com/tfr/math/vector/Vector.java
@@ -4,10 +4,17 @@ import com.tfr.math.point.Point;
 
 public interface Vector {
 
+    /**
+     * The starting point of the vector
+     * @return Point
+     */
     Point point1();
+
     Point point2();
 
     double xProjection();
+
     double yProjection();
+
     double zProjection();
 }
