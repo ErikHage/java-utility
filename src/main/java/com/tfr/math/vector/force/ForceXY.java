d0c53988fe1109c302e6099c5db2284a8da6e4a4
Erik Hage <ehage4@gmail.com>
2023-12-10 20:39:54 -0500

    remove unused imports

--- src/main/java/com/tfr/math/vector/force/ForceXY.java
+++ src/main/java/com/tfr/math/vector/force/ForceXY.java
@@ -1,7 +1,5 @@
 package com.tfr.math.vector.force;
 
-import com.tfr.math.trig.AngleUnits;
-import com.tfr.math.trig.TrigMath;
 import com.tfr.math.vector.VectorXY;
 
 public class ForceXY extends VectorXY {
