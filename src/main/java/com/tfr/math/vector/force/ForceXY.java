69039fab3ccb2ee26e07ee8e5c6a514b03950e04
Erik Hage <ehage4@gmail.com>
2023-12-11 20:40:46 -0500

    javadocs

--- src/main/java/com/tfr/math/vector/force/ForceXY.java
+++ src/main/java/com/tfr/math/vector/force/ForceXY.java
@@ -2,6 +2,9 @@ package com.tfr.math.vector.force;
 
 import com.tfr.math.vector.VectorXY;
 
+/**
+ * Representation of a 2-dimensional force vector
+ */
 public class ForceXY extends VectorXY {
 
     public ForceXY(double x, double y) {
