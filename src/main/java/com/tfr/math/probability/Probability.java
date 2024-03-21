32308db030ea3a9ca65f6314822ba320650c689e
Erik Hage <ehage4@gmail.com>
2024-03-21 17:54:12 -0400

    add mean method

--- src/main/java/com/tfr/math/probability/Probability.java
+++ src/main/java/com/tfr/math/probability/Probability.java
@@ -1,10 +1,21 @@
 package com.tfr.math.probability;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class Probability {
 
     public static BigDecimal probability(int numberOfFavorableOutcomes, int numberOfUnfavorableOutcomes) {
         return BigDecimal.valueOf((double) numberOfFavorableOutcomes / (numberOfFavorableOutcomes + numberOfUnfavorableOutcomes));
+    }
+
+    public static double mean(double... nums) {
+        double total = 0;
+        for (double num : nums) {
+            total += num;
+        }
+
+        return total / nums.length;
     }
 }
