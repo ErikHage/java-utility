14c449321755667e8f7d0e1c5f2ee634df578f2c
Erik Hage <ehage4@gmail.com>
2024-09-15 09:49:01 -0400

    add if/else stx example

--- src/test/java/com/tfr/xml/StxTransformerTest.java
+++ src/test/java/com/tfr/xml/StxTransformerTest.java
@@ -58,4 +58,46 @@ class StxTransformerTest {
 
         assertEquals(expectedOutput, output);
     }
+
+    @Test
+    void shouldHandleConditionalIf() throws TransformerException {
+        final String input1 = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <root><a>a</a></root>
+                """;
+        final String expectedOutput1 = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <root><a>a</a><c>1</c></root>
+                """;
+        final String input2 = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <root><a>b</a></root>
+                """;
+        final String expectedOutput2 = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <root><a>b</a><c>c</c></root>
+                """;
+        final String stx = """
+                <stx:transform version="1.0" pass-through="all" xmlns:stx="http://stx.sourceforge.net/2002/ns">
+                    <stx:template match="root/a">
+                        <stx:copy>
+                            <stx:process-attributes/>
+                            <stx:process-children/>
+                        </stx:copy>
+                        <stx:if test=". = 'b'">
+                            <c>c</c>
+                        </stx:if>
+                        <stx:else>
+                            <c>1</c>
+                        </stx:else>
+                    </stx:template>
+                </stx:transform>
+                """;
+
+        final String output1 = transformer.transform(stx, input1);
+        final String output2 = transformer.transform(stx, input2);
+
+        assertEquals(expectedOutput1, output1);
+        assertEquals(expectedOutput2, output2);
+    }
 }
